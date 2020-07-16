f47887adfcd2cd2868abc74eb042586a826ccf13
HichuYamichu <hichuyamichu@gmail.com>
2020-07-16 19:05:42 +0200

    somewhat ok woker pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod api;
-pub mod worker_pool;
 pub mod queue;
+pub mod worker;
 
 pub type GenericError = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T> = std::result::Result<T, GenericError>;
